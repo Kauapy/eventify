980d4049708278e26c40b18138468e7c42529098
Kauapy <kauadecarvalho20@gmail.com>
2025-06-04 18:24:27 -0300

    Finallizar Header da página principal

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './Home.css'
+import { Link } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 function Home() {
+
+const handleSignOut = () => {
+  localStorage.removeItem('token')
+  Navigate("/login")
+}
+
   return (
     <div>
-        <h1>Bem vindo ao Eventify</h1>
+        <header className='header-container'>
+            <h1 className='titulo-principal'>Eventify</h1>
+            <div className='links-container'>
+              <Link className='link02'>Home</Link>
+              <Link className='link02'>Events</Link>
+            </div>
+            <Link onClick={handleSignOut} className='Sign-Out'>Sign out</Link>
+        </header>
     </div>
   )
 }
